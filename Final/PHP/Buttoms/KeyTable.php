1251a91c92728aac25a58380e9ce627f23d45e23
Quan Zheng <qzheng22@uic.edu>
2017-08-21 22:56:39 -0500

    Good to know that, thank you phil

--- Final/PHP/Buttoms/KeyTable.php
+++
@@ -1,4 +0,0 @@
-<!-Just create a buttom reserved for the keytable.-!>
-<form form method='post'>
-  <input style="height:40px;width:150px" type = "Submit" name="sk" value = "Display Key Information">
-</form>
--- Final/PHP/Buttoms/ShowTransTable.php
+++
@@ -1,4 +0,0 @@
-<!-Just create a buttom reserved for the keytable.-!>
-<form form method='post'>
-  <input style="height:40px;width:150px" type = "Submit" name="tf" value = "Transformer Table">
-</form>
